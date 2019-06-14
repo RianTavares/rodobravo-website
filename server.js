7ad65c4cf35fd59bfeef1f58cedae4e3a2c7c192
Rian Tavares <rian.tavares@live.com>
2019-06-14 20:19:42 -0300

    testing react build to deploy

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/api/mensagem', (req, res) => {
   res.send({ express: 'Hello From Express' });

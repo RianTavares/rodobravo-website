817f3f0f023fc3d7d0f03a49dd68d3adced084f7
Rian Tavares <rian.tavares@live.com>
2019-06-14 21:03:00 -0300

    testing deploy config

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const favicon = require('express-favicon');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(path.join(__dirname, 'build')));
+
+ app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.get('/api/mensagem', (req, res) => {
   res.send({ express: 'Hello From Express' });

46d5cae5774104f287b5656afdfc96d92bd60778
Rian Tavares <rian.tavares@live.com>
2019-07-24 01:44:12 -0300

    removing env sent from back

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const API_MAP = process.env.API_MAP;
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // An api endpoint that returns a short list of items
 app.get('/api/map', (req,res) => {
-    var mapKey = API_MAP;
-    console.log('API_MAPA: ', mapKey)
-    res.send({ MAP_KEY: mapKey });
     console.log('chave enviada');
 });
 
